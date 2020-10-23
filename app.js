a94173a02b96d532ad6bb1d8120e4b60f80671eb
Tillmann Weimer <tillmann1970@gmail.com>
2020-10-23 10:29:53 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const multiply = document.querySelector("multiply")
+const devide = document.querySelector("devide")
+const minus = document.querySelector("minus")
+const plus = document.querySelector("plus")
+
+console.log(multiply)
